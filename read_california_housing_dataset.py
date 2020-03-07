6754e56740a569dd30566a540212480b470108ba
Molzen <33835030+Molzen@users.noreply.github.com>
2020-03-07 16:33:55 +0100

    Making the code better, fixing small things

--- read_california_housing_dataset.py
+++ read_california_housing_dataset.py
@@ -2,44 +2,23 @@
 # https://scikit-learn.org/dev/datasets/index.html#california-housing-dataset
 # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_california_housing.html
 
+#%%
 from sklearn.datasets.california_housing import fetch_california_housing
 import matplotlib.pyplot as plt
+import numpy as np
 
 cal_housing = fetch_california_housing()
 X, y = cal_housing.data, cal_housing.target
-names = cal_housing.feature_names
-
-#%% Show example data
-
-Nsamples = X.shape[0]
-
-n = 42
-xn = X[n, :]
-
-# Input values
-for i,val in enumerate(xn):
-    print(names[i], val)
-
-# Target value
-print('Target :', y[n])
-
-#%% 1D data plot
-
-plt.plot(X[:,0], y, '.', markersize=1)
-plt.title(names[0])
-
-
-# %%
-#%% New names for variables
-import numpy as np
 median_income = X[:, np.newaxis, 0]
 median_house_value = y
 
 plt.plot(median_income, median_house_value, '.', markersize = 1)
-plt.title("Median house value for California districts i forhold til MedInc median income in block")
+plt.title("Median house value for California districts relative to MedInc median income in block")
 plt.xlabel("Median income")
 plt.ylabel("Median house value")
 plt.show()
+
+#%%
 
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
@@ -58,20 +37,25 @@ print('Coefficients: \n', regr.coef_)
 # The mean squared error
 print('Mean squared error: %.2f'
       % mean_squared_error(median_house_value, median_house_value_pred))
-# The coefficient of determination: 1 is perfect prediction
-
-plt.show()
+#%%
 
 residual_values = median_house_value-median_house_value_pred
 
 plt.plot(median_income, residual_values, '.', markersize = 1)
-plt.title("residual-værdierne som funktion af feature-værdi")
+plt.title("Residual Plot")
 plt.xlabel("Median income")
-plt.ylabel("Residual værdier")
+plt.ylabel("Residual")
 plt.show()
 
+#%%
+
 plt.hist(residual_values)
+plt.title("Histogram of residual values")
+plt.xlabel("Residual")
+plt.ylabel("Median income")
 plt.show()
+
+#%%
 
 # Train the model using the training sets
 regr.fit(X, median_house_value)
@@ -82,3 +66,5 @@ median_house_value_pred = regr.predict(X)
 # The mean squared error
 print('Mean squared error: %.2f'
       % mean_squared_error(median_house_value, median_house_value_pred))
+
+# %%
